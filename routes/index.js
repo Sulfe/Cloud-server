b6cf8c6aa914fc7d8f58b833c2821c9e015d3d4c
Sulfe <88019008+Sulfe@users.noreply.github.com>
2022-11-16 17:59:20 +0900

    1st design

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var app = express();
+var fs = require("fs");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Express" });
 });
 
 module.exports = router;
+
+app.get("public/images", function (req, res) {
+    fs.readFile("002.jpg", function (error, data) {
+        if (error) {
+            res.send();
+        }
+        res.send(data);
+    });
+});
